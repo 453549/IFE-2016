a5ef06ede68584f2d098746300ae9334d302324d
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-11 23:19:18 +0800

    project39

---
+++ task3/serial1/Project39/js/main.js
@@ -0,0 +1,62 @@
+
+var tableAction = {
+    
+    lockCount: 0,
+
+    createTable: function() {
+        var firstTr = document.createElement("tr");
+        for (var i = 0; i < data.colName.length; i++) {
+            var curTh = document.createElement("th");
+            curTh.innerHTML = (data.colName)[i];
+            firstTr.appendChild(curTh);
+        }
+        $("#target-form").appendChild(firstTr);
+        for (var i = 0; i < data.rowData.length; i++) {
+            var curTr = document.createElement("tr");
+            var curData = [];
+            for (var j = 0; j < data.colName.length; j++) {
+                var curTd = document.createElement("td");
+                curTd.innerHTML = ((data.rowData)[i])[j];
+                curData.push(((data.rowData)[i])[j]);
+                curTr.appendChild(curTd);
+            }
+            $("#target-form").appendChild(curTr);
+        }
+    },
+    
+    addLockTableRow: function(rowId) {
+        var lockedForm = $("#locked-form");
+        var curTr = document.createElement("tr");
+        var AllTr = $("tr");
+        curTr.innerHTML = AllTr[rowId + tableAction.lockCount].innerHTML;
+        lockedForm.appendChild(curTr);
+        tableAction.lockCount++;
+    },
+    
+    checkLockTable: function(rowId) {    
+        var lockedForm = $("#locked-form");
+        var targetForm = $("#target-form");
+        var toTop = document.documentElement.scrollTop + document.body.scrollTop;
+        if (toTop > targetForm.offsetTop + targetForm.clientHeight || toTop < targetForm.offsetTop) {
+            lockedForm.innerHTML = "";
+            lockedForm.style.position = "";
+            tableAction.lockCount = 0;
+        }
+        else {
+            if (lockedForm.style.position == "fixed") return;
+            if (document.documentElement.scrollTop + document.body.scrollTop > targetForm.offsetTop) {
+                lockedForm.style.position = "fixed";
+                lockedForm.style.left = targetForm.offsetLeft;
+                tableAction.addLockTableRow(0);
+            }
+        }
+    }
+}
+
+window.onscroll = function() {
+    tableAction.checkLockTable(0);
+}
+
+window.onload = function() {
+    tableAction.createTable();
+}
