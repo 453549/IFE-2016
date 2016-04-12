235371fd25292b992b6eb2f17240e1c2b80d022f
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-12 22:32:28 +0800

    encapsulated project39

--- task3/serial1/Project39/js/main.js
+++ task3/serial1/Project39/js/main.js
@@ -1,68 +1,80 @@
-var lockedForm = $("#locked-form");
-var targetForm = $("#target-form");
-lockedForm.style.left = targetForm.offsetLeft + "px";
-
-
-var tableAction = {
+function tableForm(data) {
     
-    lockCount: 0,
+    var that = this;
+    this.mainTable = document.createElement("table");
+    this.lockedForm = document.createElement("table");
+    this.lockCount = 0;
 
-    createTable: function() {
+    this.createTable = function() {
+        that.mainTable.setAttribute("class", "target-form");
+        that.lockedForm.setAttribute("class", "locked-form");
+        that.lockedForm.style.left = that.mainTable.offsetLeft + "px";
         var firstTr = document.createElement("tr");
         for (var i = 0; i < data.colName.length; i++) {
             var curTh = document.createElement("th");
             curTh.innerHTML = (data.colName)[i];
             firstTr.appendChild(curTh);
         }
-        $("#target-form").appendChild(firstTr);
+        that.mainTable.appendChild(firstTr);
         for (var i = 0; i < data.rowData.length; i++) {
             var curTr = document.createElement("tr");
-            var curData = [];
             for (var j = 0; j < data.colName.length; j++) {
                 var curTd = document.createElement("td");
                 curTd.innerHTML = ((data.rowData)[i])[j];
-                curData.push(((data.rowData)[i])[j]);
                 curTr.appendChild(curTd);
             }
-            $("#target-form").appendChild(curTr);
+            that.mainTable.appendChild(curTr);
         }
-    },
+    };
     
-    addLockTableRow: function(rowId) {
-        var lockedForm = $("#locked-form");
+    this.addLockTableRow = function(rowId) {
         var curTr = document.createElement("tr");
         var AllTr = $("tr");
-        curTr.innerHTML = AllTr[rowId + tableAction.lockCount].innerHTML;
-        lockedForm.appendChild(curTr);
-        tableAction.lockCount++;
-    },
+        curTr.innerHTML = AllTr[rowId + that.lockCount].innerHTML;
+        that.lockedForm.appendChild(curTr);
+        that.lockCount++;
+    };
     
-    checkLockTable: function(rowId) {    
-        lockedForm.style.left = targetForm.offsetLeft + "px";
+    this.checkLockTable = function(rowId) { 
+        that.lockedForm.style.left = that.mainTable.offsetLeft + "px";
         var toTop = document.documentElement.scrollTop + document.body.scrollTop;
-        if (toTop > targetForm.offsetTop + targetForm.clientHeight || toTop < targetForm.offsetTop) {
-            lockedForm.innerHTML = "";
-            lockedForm.style.position = "";
-            tableAction.lockCount = 0;
+        if (toTop > that.mainTable.offsetTop + that.mainTable.clientHeight || toTop < that.mainTable.offsetTop) {
+            that.lockedForm.innerHTML = "";
+            that.lockedForm.style.position = "";
+            that.lockCount = 0;
         }
         else {
-            if (lockedForm.style.position == "fixed") return;
-            if (document.documentElement.scrollTop + document.body.scrollTop > targetForm.offsetTop) {
-                lockedForm.style.position = "fixed";
-                tableAction.addLockTableRow(rowId);
+            if (that.lockedForm.style.position == "fixed") return;
+            if (document.documentElement.scrollTop + document.body.scrollTop > that.mainTable.offsetTop) {
+                that.lockedForm.style.position = "fixed";
+                that.addLockTableRow(rowId);
             }
         }
+    };
+    
+    this.delegateEvent = function() {
+        addEvent(window, "scroll", function() {
+            return function(){return that.checkLockTable(0)};
+        }());
+        addEvent(window, "resize", function() {
+            return function(){return that.checkLockTable(0)};
+        }());
+    };
+    
+    this.init = function() {
+        that.createTable();
+        that.delegateEvent();
+        document.body.appendChild(that.mainTable);
+        document.body.appendChild(that.lockedForm);
     }
 }
 
-window.onscroll = function() {
-    tableAction.checkLockTable(0);
-}
-
-window.onresize = function() {
-    tableAction.checkLockTable(0);
-}
-
 window.onload = function() {
-    tableAction.createTable();
+    var newForm = new tableForm(data);
+    newForm.init(); 
+    
+    //add some craps...
+    for (var i = 0; i < 40; i++) {
+        document.body.appendChild(document.createElement("br"));
+    }
 }
